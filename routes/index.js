a5a1720ec5cf67ec3a29af161689626fd14a00fd
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-25 22:12:02 -0300

    Set up routing

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send("Hello, world!");
+router.get('/posts', function (req, res, next) {
+    res.send("Sending posts.");
+});
+
+router.get('/posts/:id', function (req, res, next) {
+    res.send("Sending details about an specific post.");
+});
+
+router.post("posts/:id/comments/create", function (req, res, next) {
+    res.send("Creating new post.");
+});
+
+router.post("users/create", function (req, res, next) {
+    res.send("Creating new user.");
+});
+
+router.get("users/login", function (req, res, next) {
+    res.send("Authenticating user.");
 });
 
 module.exports = router;

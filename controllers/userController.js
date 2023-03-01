f03303fa462afe4049bd3e5215776d001c00bed4
CantarinoG <guilhermecantarino1709@gmail.com>
2023-03-01 01:10:04 -0300

    Add createUser controller function

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,43 @@ const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 exports.createUser = (req, res, next) => {
-    res.send("Creating new user.");
+    jwt.verify(req.token, process.env.JWT_KEY, (err, authData) => {
+        if(err) {
+
+            const username = req.body.username;
+            const password = req.body.password;
+            const confirm = req.body.confirm;
+
+            User.findOne({ username }).exec(function(err, user) {
+                if(err) return res.status(500).json({ error: "Something went wrong. Could not check username availability", status: 500, userLoggedIn: false });
+
+                const alerts = [];
+
+                if(user != null) alerts.push("This username is already in use.");
+                if(!username) alerts.push("Username is required.");
+                if(!password || !confirm) alerts.push("Password is required.");
+                if(password != confirm) alerts.push("Passwords do not match.");
+
+                if(alerts.length) return res.status(400).json({ status: 400, alerts, userLoggedIn: false });
+
+                bcrypt.hash(password, 10, (err, hashedPassword) => {
+                    if (err) return res.status(500).json({ status: 500, error: "Could not proccess password.", userLoggedIn: false });
+                    const user = new User({
+                        username,
+                        password: hashedPassword
+                    }).save(err => {
+                        if (err) return res.status(500).json({ status: 500, error: "Could not save user.", userLoggedIn: false });
+                        return res.status(200).json({ status: 200, userLoggedIn: true, success: "User created!"});
+                    });
+                })
+
+            });
+
+            }
+        else {
+            return res.status(403).json({error: "User is already logged in.", status: 403, userLoggedIn: true});
+            }
+    })
 }
 
 exports.logInUser = (req, res, next) => {
